d256e728159c47d253f5e3d80d23c203cc35fbbb
EC2 Default User <ec2-user@ip-172-31-23-97.ec2.internal>
2021-02-09 08:22:45 +0000

    コメントといいね機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
 devise_for :users
 resources :post_images, only: [:new, :create, :index, :show, :destroy]
 root to: "homes#top"
+resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
+
 end
