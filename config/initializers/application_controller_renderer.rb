304c4b5f0be07ed6a445d501b5ac3b6a1ee40cf5
EC2 Default User <ec2-user@ip-172-31-23-97.ec2.internal>
2021-02-09 01:27:32 +0000

    投稿フォームの追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -1,5 +1,5 @@
 # Be sure to restart your server when you modify this file.
-
+Refile.secret_key = '2cbce4f63e9a9041d0565b47c2cbda28fbaadd905bb267fe40dc4ec36b6ec8b4ab5019f4c70e87678010facc100b9fc0e32a93b51b04bb5afe54724d8e9f390b'
 # ActiveSupport::Reloader.to_prepare do
 #   ApplicationController.renderer.defaults.merge!(
 #     http_host: 'example.org',
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+devise_for :users
+resources :post_images, only: [:new, :create, :index, :show, :destroy]
+root to: "homes#top"
 end
